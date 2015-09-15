2f2216517698dc56376ff25cc6e9ec6aa4e40ce6
unknown <leocwolter@gmail.com>
2015-09-15 20:48:56 -0300

    Spike de resource injection(não funcionando), usar cdi

--- src/br/com/acommerce/book/BooksBean.java
+++ src/br/com/acommerce/book/BooksBean.java
@@ -1,16 +1,23 @@
 package br.com.acommerce.book;
 
+import java.sql.SQLException;
 import java.util.List;
 
+import javax.annotation.Resource;
 import javax.faces.bean.ManagedBean;
-
-import br.com.acommerce.infra.ConnectionFactory;
+import javax.sql.DataSource;
 
 @ManagedBean
 public class BooksBean {
 	
-	public List<Book> getList(){
-		BookDAO books = new BookDAO(ConnectionFactory.getConnection());
+	@Resource(name="java:comp/acommerce")
+	private DataSource dataSource;
+	
+	@Resource(name="java:comp/jdbc/acommerce")
+	private DataSource anotherDataSource;
+	
+	public List<Book> getList() throws SQLException{
+		BookDAO books = new BookDAO(dataSource.getConnection());
 		return books.all();
 	}
 	
